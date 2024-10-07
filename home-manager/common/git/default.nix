a9f289c76ec88bc728fc860f72e1cebef974f38e
Artur Roszczyk <artur.roszczyk@gmail.com>
2024-10-07 08:49:50 +0200

    GH helper

--- home-manager/common/git/default.nix
+++ home-manager/common/git/default.nix
@@ -2,7 +2,7 @@
   # basic configuration of git, please change to your own
   programs.git = {
     enable = true;
-    userName = "artur roszczyk";
+    userName = "Artur Roszczyk";
     userEmail = "sevos@hey.com";
 
     includes = [
@@ -18,7 +18,8 @@
       core.autocrlf = "input";
       core.excludesfile = "$HOME/.config/git/gitignore_global";
       init.defaultBranch = "main";
-      credential."https://github.com".helper = "gh auth git-credential";
     };
   };
+
+  programs.gh.enable = true;
 }

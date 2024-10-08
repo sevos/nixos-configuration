8f6e4824e2ec699bbc0d0676dcdd8226be29d9cb
Artur Roszczyk <artur.roszczyk@gmail.com>
2024-10-08 15:43:35 +0200

    remove styling that did not work

--- nixos/common/optional/bootloader/uefi.nix
+++ nixos/common/optional/bootloader/uefi.nix
@@ -1,12 +1,9 @@
-{pkgs, ...}:
 {
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.gfxmodeEfi = "2560x1600";
-  boot.loader.grub.font = "${pkgs.hack-font}/share/fonts/hack/Hack-Regular.ttf";
-  boot.loader.grub.fontSize = 36;
   boot.loader.grub.useOSProber = true;
   boot.loader.efi.canTouchEfiVariables = true;
 }

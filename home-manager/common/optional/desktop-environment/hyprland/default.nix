9fbf9b44c7a1de9f2d3c9d96046c6de5635d79aa
Artur Roszczyk <artur.roszczyk@gmail.com>
2024-10-08 19:22:54 +0200

    Fix hyprland to stable

--- home-manager/common/optional/desktop-environment/hyprland/default.nix
+++ home-manager/common/optional/desktop-environment/hyprland/default.nix
@@ -20,7 +20,7 @@
     # Whether to enable Hyprland wayland compositor
     enable = true;
     # The hyprland package to use
-    package = pkgs.unstable.hyprland;
+    package = pkgs.hyprland;
     # Whether to enable XWayland
     xwayland.enable = true;
 

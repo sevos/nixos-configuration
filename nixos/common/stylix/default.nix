ec77d5021ecc6e06d29075ea25d2f7134ec58c0b
Artur Roszczyk <artur.roszczyk@gmail.com>
2024-10-08 22:05:57 +0200

    Stylix fonts

--- nixos/common/stylix/default.nix
+++ nixos/common/stylix/default.nix
@@ -3,7 +3,31 @@
   imports = [
   ];
 
-  stylix.enable = true;
-  stylix.image = ../../../home-manager/sevos/wallpaper.jpg;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/tomorrow-night.yaml";
+  stylix = {
+    enable = true;
+    image = ../../../home-manager/sevos/wallpaper.jpg;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/tomorrow-night.yaml";
+
+    fonts = {
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+
+      monospace = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans Mono";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
+  };
 }

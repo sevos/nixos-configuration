b3c9b9369b0882563ece0933bdd9e616ace95e2a
Artur Roszczyk <artur.roszczyk@gmail.com>
2024-10-13 22:19:01 +0200

    WIP

--- home-manager/common/terminal/default.nix
+++ home-manager/common/terminal/default.nix
@@ -10,10 +10,22 @@
     enable = true;
     # custom settings
     settings = {
-      import = [
-        pkgs.alacritty-theme.tokyo-night
-      ];
       env.TERM = "xterm-256color";
+      bell = {
+        animation = "Ease";
+        duration = 200;
+        color = "#7aa2f7";
+      };
+      cursor = {
+        style = {
+          shape = "Beam";
+        };
+      };
+
+      window = {
+        padding = { x = 10; y = 10; };
+      };
+
       scrolling.multiplier = 5;
       selection.save_to_clipboard = true;
       mouse = {

a73bf8d72bb7120e1b906647e17b5a91d06da2c9
Artur Roszczyk <sevos@hey.com>
2024-10-22 10:19:18 +0200

    Move opacity overrides

--- home-manager/common/desktop-environment/hyprland/hyprland.nix
+++ home-manager/common/desktop-environment/hyprland/hyprland.nix
@@ -206,7 +206,9 @@
 		windowrulev2 = [
 			# no opcacity for windows with "Meet" in title
 			"opacity 1.0 override,title:^meet\.google\.com.*$"
+			"opacity 1.0 override,title:^Meet.*$"
 			"opacity 1.0 override,title:^YouTube.*$"
+			"opacity 1.0 override,title:^.*youtube.com$"
 			"suppressevent maximize, class:.*" # You'll probably like this.
 		];
   };

8447cf40707512168d91c2532e44323e4c26a0c9
Artur Roszczyk <sevos@hey.com>
2024-10-23 14:04:16 +0200

    Add swappy to hyprland for screenshot annotation

--- home-manager/common/desktop-environment/hyprland/hyprshot.nix
+++ home-manager/common/desktop-environment/hyprland/hyprshot.nix
@@ -3,12 +3,13 @@
 {
   home.packages = with pkgs; [
     hyprshot
+    swappy
   ];
 
   wayland.windowManager.hyprland.settings = {
     bind = [
-      "$mainMod, F4, exec, hyprshot -m window --clipboard-only"
-      "$mainMod SHIFT, F4, exec, hyprshot -m region --clipboard-only"
+      "$mainMod, F4, exec, hyprshot --freeze -m window --clipboard-only --raw | swappy -f -"
+      "$mainMod SHIFT, F4, exec, hyprshot --freeze -m region --clipboard-only --raw | swappy -f -"
     ];
   };
 }

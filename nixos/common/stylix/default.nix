8e3cb16b6292388dd8424360d61b295fbe190378
Artur Roszczyk <sevos@hey.com>
2024-11-27 23:12:24 +0100

    Typo

--- nixos/common/stylix/default.nix
+++ nixos/common/stylix/default.nix
@@ -6,7 +6,7 @@
   stylix = {
     enable = true;
     polarity = "dark";
-    image = lib.mkDefault ../../../style/wallpapers/b0ek3ocp4rud1.png
+    image = lib.mkDefault ../../../style/wallpapers/b0ek3ocp4rud1.png;
     
     base16Scheme = ../../../style/tokyo-dark.yaml;
 

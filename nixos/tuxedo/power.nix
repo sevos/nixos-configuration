9e3fbb42f31b544beeddfa754cab147cdc3bfe2b
Artur Roszczyk <artur.roszczyk@gmail.com>
2024-12-15 23:40:13 +0100

    More power for laptop

--- nixos/tuxedo/power.nix
+++ nixos/tuxedo/power.nix
@@ -18,10 +18,10 @@
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 100;
+      CPU_MAX_PERF_ON_BAT = 80;
 
       CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
+      CPU_BOOST_ON_BAT = 1;
 
       # Works on ThinkPads only
       # START_CHARGE_THRESH_BAT0 = 40;

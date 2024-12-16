e2a70963be26501412eabe1c59042258abc44361
Artur Roszczyk <sevos@hey.com>
2024-12-16 13:57:28 +0100

    AI config

--- home-manager/common/neovim/lua/plugins/codecompanion.lua
+++ home-manager/common/neovim/lua/plugins/codecompanion.lua
@@ -5,6 +5,23 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
-    config = true
+    config = true,
+    adapters = {
+      ollama = function()
+        return require("codecompanion.adapters").extend("ollama", {
+          env = {
+            url = "http://10.10.11.10:11434",
+--            api_key = "OLLAMA_API_KEY",
+          },
+--          headers = {
+--            ["Content-Type"] = "application/json",
+--            ["Authorization"] = "Bearer ${api_key}",
+--          },
+          parameters = {
+            sync = true,
+          },
+        })
+      end,
+    },
   }
 }

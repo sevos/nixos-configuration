e228fd1dca7c8886cc1dc99855f6aecff7db427c
Artur Roszczyk <sevos@hey.com>
2024-12-21 00:07:02 +0100

    feat(neovim): enable rspec and minitest adapters for neotest
    Added support for rspec and minitest testing adapters in neotest config.
    This includes enabling the neotest-minitest plugin via Nix package.

--- home-manager/common/neovim/utils/neotest.nix
+++ home-manager/common/neovim/utils/neotest.nix
@@ -1,7 +1,15 @@
+{ pkgs, ... }:
 {
   programs.nixvim = {
     plugins.neotest = {
       enable = true;
+      adapters = {
+        rspec.enable = true;
+        minitest = {
+          enable = true;
+          package = pkgs.unstable.vimPlugins.neotest-minitest;
+        };
+      };
     };
   };
 }

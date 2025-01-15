f8d1a0ab6bd994e7dbe74190f8d69d2b29065eb1
Artur Roszczyk <sevos@hey.com>
2025-01-15 17:05:05 +0100

    Enable SSH server on peon

--- nixos/peon/configuration.nix
+++ nixos/peon/configuration.nix
@@ -183,7 +183,7 @@
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
-    enable = false;
+    enable = true;
     settings = {
       # Opinionated: forbid root login through SSH.
       PermitRootLogin = "no";

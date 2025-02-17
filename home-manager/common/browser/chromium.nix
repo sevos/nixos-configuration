a1120d6b0b8339e3264967ddc4a0e0f1713f161a
Artur Roszczyk <sevos@hey.com>
2025-02-17 23:52:40 +0100

    Add ChatGPT plugin to Chromium

--- home-manager/common/browser/chromium.nix
+++ home-manager/common/browser/chromium.nix
@@ -5,6 +5,7 @@
       "aeblfdkhhhdcdjpifhhbdiojplfjncoa" # 1password
       "ddkjiahejlhfcafbddmgiahcphecmpfh" # uBlock Origin Lite
       "kbfnbcaeplbcioakkpcpgfkobkghlhen" # Grammarly
+      "ejcfepkfckglbgocfkanmcdngdijcgld" # ChatGPT search
     ];
   };
 }
